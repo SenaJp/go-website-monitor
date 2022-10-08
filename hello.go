17c9ec19d3bbdf46e9146c575b5316d3dd147cb7
João Pedro Gomes <86388032+JP-dev99@users.noreply.github.com>
2022-10-08 19:21:44 -0300

    conditionals

--- hello.go
+++ hello.go
@@ -2,23 +2,41 @@ package main
 
 import "fmt"
 
+func main() {
+	nome := "JP"
+	versao := 1.1
 
-func main()	{
-	 nome := "JP"
-	 versao := 1.1
+	fmt.Println("Hi, sr.", nome)
+	fmt.Println("Version", versao)
 
-	fmt.Println("Olá, sr.", nome)
-	fmt.Println("Este programa está na versão", versao)
-
-	fmt.Println("0- Sair do programa")
-	fmt.Println("1- Iniciar monitoramento")
-	fmt.Println("2- Exibir logs")
-
+	fmt.Println("0- Exit program")
+	fmt.Println("1- Start monitoring")
+	fmt.Println("2- Show logs")
 
 	var command int
 	fmt.Scan(&command)
 
 	fmt.Println("O endereço da variável é", &command)
 	fmt.Println("o comando escolhido foi", command)
-}
 
+	// if command == 1 {
+	// 	fmt.Println("Monitorando")
+	// } else if command == 2 {
+	// 	fmt.Println("Show Logs")
+	// } else if command == 3 {
+	// 	println("Exit")
+	// } else {
+	// 	fmt.Println("Command not found")
+	// }
+
+	switch command {
+	case 1:
+		fmt.Println("Monitoring")
+	case 2:
+		fmt.Println("Show logs")
+	case 3:
+		fmt.Println("Exit")
+	default:
+		fmt.Println("Command not found")
+	}
+}
