2a7cf3d86686f5f43c585c9a09c8e6eb3d8a1487
João Pedro Gomes <86388032+JP-dev99@users.noreply.github.com>
2022-10-08 19:33:22 -0300

    funcs

--- hello.go
+++ hello.go
@@ -1,42 +1,47 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
-	nome := "JP"
-	versao := 1.1
 
-	fmt.Println("Hi, sr.", nome)
-	fmt.Println("Version", versao)
-
-	fmt.Println("0- Exit program")
-	fmt.Println("1- Start monitoring")
-	fmt.Println("2- Show logs")
-
-	var command int
-	fmt.Scan(&command)
-
-	fmt.Println("O endereço da variável é", &command)
-	fmt.Println("o comando escolhido foi", command)
-
-	// if command == 1 {
-	// 	fmt.Println("Monitorando")
-	// } else if command == 2 {
-	// 	fmt.Println("Show Logs")
-	// } else if command == 3 {
-	// 	println("Exit")
-	// } else {
-	// 	fmt.Println("Command not found")
-	// }
+	showIntroduction()
+	showMenu()
+	command := readCommand()
 
 	switch command {
 	case 1:
 		fmt.Println("Monitoring")
 	case 2:
 		fmt.Println("Show logs")
-	case 3:
+	case 0:
 		fmt.Println("Exit")
+		os.Exit(0)
 	default:
 		fmt.Println("Command not found")
+		os.Exit(-1)
 	}
 }
+
+func showIntroduction() {
+	nome := "JP"
+	versao := 1.1
+	fmt.Println("Hi, sr.", nome)
+	fmt.Println("Version", versao)
+}
+
+func showMenu() {
+	fmt.Println("0- Exit program")
+	fmt.Println("1- Start monitoring")
+	fmt.Println("2- Show logs")
+}
+
+func readCommand() int {
+	var command int
+	fmt.Scan(&command)
+	fmt.Println("o comando escolhido foi", command)
+
+	return command
+}
+
+
